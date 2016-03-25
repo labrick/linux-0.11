781f37394dc9a50012c761f77d3fdbe79a023530
labrick <yananback@gmail.com>
2016-03-25 21:28:38 +0800

    块设备Makefile
    1. 主要内容比较基本
    2. 有个AR的二进制打包工具的利用，将.o文件最后打包成了.a库文件

--- include/sys/times.h
+++ include/sys/times.h
@@ -4,10 +4,10 @@
 #include <sys/types.h>
 
 struct tms {
-	time_t tms_utime;
-	time_t tms_stime;
-	time_t tms_cutime;
-	time_t tms_cstime;
+	time_t tms_utime;   // 进程用户运行时间
+	time_t tms_stime;   // 内核(系统)时间
+	time_t tms_cutime;  // 子进程用户运行时间
+	time_t tms_cstime;  // 子进程系统运行时间
 };
 
 extern time_t times(struct tms * tp);
--- include/sys/utsname.h
+++ include/sys/utsname.h
@@ -4,11 +4,11 @@
 #include <sys/types.h>
 
 struct utsname {
-	char sysname[9];
-	char nodename[9];
-	char release[9];
-	char version[9];
-	char machine[9];
+	char sysname[9];    // 当前运行系统的名称
+	char nodename[9];   // 网络节点名称(主机名)
+	char release[9];    // 当前操作系统发行级别
+	char version[9];    // 操作系统版本号
+	char machine[9];    // 系统运行的硬件类型名称
 };
 
 extern int uname(struct utsname * utsbuf);
