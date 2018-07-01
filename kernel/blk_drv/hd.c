a69b8eeda9d90e2953a42bbe48842662518a0acc
labrick <yananback@gmail.com>
2018-07-01 16:15:12 +0800

    忘记注释什么了

--- kernel/blk_drv/hd.c
+++ kernel/blk_drv/hd.c
@@ -245,75 +245,117 @@ int sys_setup(void * BIOS)
 	return (0);
 }
 
+//// 判断并循环等待硬盘控制器就绪。
+// 读硬盘控制器状态寄存器端口HD_STATUS(0x1f7)，循环检测其中的驱动器就绪比特位(位6)
+// 是否被置位并且控制器忙位(位7)是否被复位。如果返回值retries为0，则表示等待控制器
+// 空闲的时间已经超时而发生错误，若返回值不为0则说明在等待(循环)时间期限内控制器
+// 回到空闲状态，OK!
+// 实际上，我们仅需检测状态寄存器忙位(位7)是否为1来判断控制器是否处于忙状态，驱动
+// 器是否就绪(即位6是否为1)与控制器的状态无关。因此我们可以把第165行语句改写成：
+// "while (--retries && (inb_p(HD_STATUS)&0x80));"另外，由于现在的PC机速度都很快，
+// 因此我们可以把等待的循环次数再加大一些，例如再增加10倍！
 static int controller_ready(void)
 {
-	int retries=10000;
+	int retries=10000;      // 等待时间，如果那么长时间还没就绪，也代表控制器出错了
 
 	while (--retries && (inb_p(HD_STATUS)&0xc0)!=0x40);
-	return (retries);
+	return (retries);           // 返回等待循环次数。
 }
 
+//// 检测硬盘执行命令后的状态。(win表示温切斯特硬盘的缩写)
+// 读取状态寄存器中的命令执行结果状态。返回0表示正常；1表示错误。如果执行命令出错，
+// 则需要再读错误寄存器HD_ERROR(0x1f1)
 static int win_result(void)
 {
-	int i=inb_p(HD_STATUS);
+	int i=inb_p(HD_STATUS);     // 读取状态信息
 
 	if ((i & (BUSY_STAT | READY_STAT | WRERR_STAT | SEEK_STAT | ERR_STAT))
-		== (READY_STAT | SEEK_STAT))
+		== (READY_STAT | SEEK_STAT))    // 这些位都为0则说明执行ok
 		return(0); /* ok */
-	if (i&1) i=inb(HD_ERROR);
+	if (i&1) i=inb(HD_ERROR);   // 若ERR_STAT置位ieee，则读取错误寄存器，其他情况不管？
 	return (1);
 }
 
+//// 向硬盘控制器发送命令块。
+// 参数:drive - 硬盘号(0-1)；  nsect - 读写扇区数；    sect - 起始扇区；
+//      head  - 磁头号；       cyl   - 柱面号；        cmd  - 命令码(见控制器命令列表)；
+//      intr_addr() - 硬盘中断处理程序中将调用的C处理函数指针。
+// 该函数在硬盘控制器就绪之后，先设置全局函数指针变量do_hd指向硬盘中断处理程序中将会调用
+// 的C处理函数，然后再发送硬盘控制字节和7字节的参数命令块。硬盘中断处理程序的代码位于
+// kernel/system_call.s程序第221行处。
+// 184行定义了一个寄存器变量__res。该变量将被保存在一个寄存器中，以便于快速访问和操作。
+// 如果想指定寄存器(例如eax)，那么我们可以把该句写成"register char __res asm("ax");"。
 static void hd_out(unsigned int drive,unsigned int nsect,unsigned int sect,
 		unsigned int head,unsigned int cyl,unsigned int cmd,
 		void (*intr_addr)(void))
 {
-	register int port asm("dx");
+	register int port asm("dx");        // 定义局部寄存器变量并放在指定寄存器dx中。
 
+// 首先对参数进行有效性检查。如果驱动器号大于1(只能是0、1)或者磁头号大于15，则程序不支持，
+// 停机。否则就判断并循环等待驱动器就绪。如果等待一段时间后仍未就绪则表示硬盘控制器出错，
+// 也停机。
 	if (drive>1 || head>15)
 		panic("Trying to write bad sector");
 	if (!controller_ready())
 		panic("HD controller not ready");
-	do_hd = intr_addr;
-	outb_p(hd_info[drive].ctl,HD_CMD);
-	port=HD_DATA;
-	outb_p(hd_info[drive].wpcom>>2,++port);
-	outb_p(nsect,++port);
-	outb_p(sect,++port);
-	outb_p(cyl,++port);
-	outb_p(cyl>>8,++port);
-	outb_p(0xA0|(drive<<4)|head,++port);
-	outb(cmd,++port);
+// 接着我们设置硬盘中断发生时将调用的C函数指针do_hd(该函数指针定义在blk.h文件的54-99行
+// 之间，请特别留意其中的81和97行)。然后在向硬盘控制器发送参数和命令之前，规定要先向
+// 控制器命令端口(0x3f6)发送一指定硬盘的控制字节，以建立相应的硬盘控制方式。该控制字节
+// 即是硬盘信息结构信息数组中的ctl字段。然后向控制器端口0x1f1-0x1f7发送7字节的参数命令块。
+	do_hd = intr_addr;                      // do_hd函数会在中断程序中被调用
+	outb_p(hd_info[drive].ctl,HD_CMD);      // 向控制寄存器输出控制字节
+	port=HD_DATA;                           // 置dx为数据寄存器端口(0x1f0)
+	outb_p(hd_info[drive].wpcom>>2,++port); // 参数：写预补偿柱面号(需除4)
+	outb_p(nsect,++port);                   // 参数：读/写扇区总数
+	outb_p(sect,++port);                    // 参数：起始扇区
+	outb_p(cyl,++port);                     // 参数：柱面号低8位
+	outb_p(cyl>>8,++port);                  // 参数：柱面号高8位
+	outb_p(0xA0|(drive<<4)|head,++port);    // 参数：驱动器号+磁头号
+	outb(cmd,++port);                       // 命令：硬盘控制命令
 }
 
+//// 等待硬盘就绪
+// 该函数循环等待主状态控制器忙标志位复位。若仅有就绪或寻道结束标志置位，则表示硬盘
+// 就绪，成功返回0。若经过一段时间仍为忙，则返回1。
 static int drive_busy(void)
 {
 	unsigned int i;
 
+// 循环读取控制器的主状态寄存器HD_STATUS，等待就绪标志位置位并且忙位复位。然后再次
+// 读取主控制器状态字节，检测其中忙位、就绪位和寻道结束位。若仅有就绪或寻道结束标志
+// 置位，则表示硬盘就绪，返回0。否则表示等待超时。于是警告显示信息。并返回1。
 	for (i = 0; i < 10000; i++)
 		if (READY_STAT == (inb_p(HD_STATUS) & (BUSY_STAT|READY_STAT)))
 			break;
-	i = inb(HD_STATUS);
+	i = inb(HD_STATUS);                     // 再取主控制器状态字节
 	i &= BUSY_STAT | READY_STAT | SEEK_STAT;
-	if (i == READY_STAT | SEEK_STAT)
+	if (i == READY_STAT | SEEK_STAT)        // 若仅有就绪或寻道结束标志则返回0
 		return(0);
-	printk("HD controller times out\n\r");
+	printk("HD controller times out\n\r");  // 否则等待超时，显示信息。并返回1
 	return(1);
 }
 
+//// 诊断复位(重新校正)硬盘控制器。
 static void reset_controller(void)
 {
 	int	i;
 
-	outb(4,HD_CMD);
-	for(i = 0; i < 100; i++) nop();
-	outb(hd_info[0].ctl & 0x0f ,HD_CMD);
+// 首先向控制寄存器端口(0x3f6)发送允许复位(4)控制字节。然后循环空操作等待一段时间
+// 让控制器执行复位操作。接着再向该端口发送正常的控制字节(不禁止重试、重读)，并等
+// 待硬盘就绪。若等待硬盘就绪超时，则显示警告信息。然后读取错误寄存器内容，若其不
+// 等于1(表示无错误)则显示硬盘控制器复位失败信息。
+	outb(4,HD_CMD);                         // 向控制寄存器端口发送复位控制字节
+	for(i = 0; i < 100; i++) nop();         // 等待一段时间
+	outb(hd_info[0].ctl & 0x0f ,HD_CMD);    // 发送正常控制字节(不禁止重试、重读)
 	if (drive_busy())
 		printk("HD-controller still busy\n\r");
 	if ((i = inb(HD_ERROR)) != 1)
 		printk("HD-controller reset failed: %02x\n\r",i);
 }
 
+//// 复位硬盘nr
+// 首先复位(重新校正)硬盘控制器。然后发送硬盘控制器命令"建立驱动器参数"，其中函数
+// recal_intr()是在硬盘中断处理程序中调用的重新校正处理函数
 static void reset_hd(int nr)
 {
 	reset_controller();
@@ -321,11 +363,18 @@ static void reset_hd(int nr)
 		hd_info[nr].cyl,WIN_SPECIFY,&recal_intr);
 }
 
+//// 意外硬盘中断调用函数
+// 发生意外硬盘中断时，硬盘中断处理程序中调用的默认C处理函数。在被调用函数指针为
+// 空时调用该函数。参见(kernel/system_call.s 241)
 void unexpected_hd_interrupt(void)
 {
 	printk("Unexpected HD interrupt\n\r");
 }
 
+//// 读写硬盘失败处理调用函数
+// 如果读扇区时的出错次数大于或等于7次时，则结束当前请求项并唤醒等待该请求的过程，
+// 而且对应缓冲区更新标志复位，表示数据没有更新。如果读一扇区时的出错次数已经大于
+// 3次，则要求执行复位硬盘控制器操作(设置复位标志)
 static void bad_rw_intr(void)
 {
 	if (++CURRENT->errors >= MAX_ERRORS)
@@ -334,50 +383,97 @@ static void bad_rw_intr(void)
 		reset = 1;
 }
 
+//// 读操作中断调用函数
+// 该函数将在硬盘读命令结束时引发的硬盘中断过程中被调用。
+// 在读命令执行后会产生硬盘中断信号，并执行硬盘中断处理程序，此时在硬盘中断处理程序
+// 中调用的C函数指针do_hd已经指向read_intr()，因此会在一次读扇区操作完成(或出错)后
+// 就会执行该函数。
 static void read_intr(void)
 {
-	if (win_result()) {
-		bad_rw_intr();
-		do_hd_request();
+// 该函数首先判断此次读命令操作是否出错。若命令结束后控制器还处于忙状态，或者命令
+// 执行错误，则处理硬盘操作失败问题，接着再次请求硬盘作复位处理并执行其他请求项。
+// 然后返回。每次读操作出错都会对当前请求项作出错次数累计，若出错次数不到最大允许
+// 出错次数的一半，则会先执行硬盘复位操作，然后再执行本次请求项处理。若出错次数已经
+// 大于等于最大允许出错次数MAX_ERRORS(7次)，则结束本次请求项的处理而去处理队列中
+// 下一个请求项。
+	if (win_result()) {         // 若控制器忙、读写错或命令执行错
+		bad_rw_intr();          // 则进行读写硬盘失败处理
+		do_hd_request();        // 再次请求硬盘作相应(复位)处理
 		return;
 	}
-	port_read(HD_DATA,CURRENT->buffer,256);
-	CURRENT->errors = 0;
-	CURRENT->buffer += 512;
-	CURRENT->sector++;
-	if (--CURRENT->nr_sectors) {
-		do_hd = &read_intr;
+// 如果读命令没有出错，则从数据寄存器端口把1个扇区的数据读到请求项的缓冲区中，并且
+// 递减请求项所需读取的扇区数值。若递减后不等于0，表示本项请求还有数据没取完，于是
+// 再次置中断调用C函数指针do_hd为read_intr()并直接返回，等待硬盘在读出另1个扇区数据
+// 后发出中断并再次调用本函数。注意：257行语句中的256是指内存字，即512字节。
+// 注意1：262行再次置do_hd指针执行read_intr()是因为硬盘中断处理程序每次调用do_hd时
+// 都会将该函数指针置空。参见system_call.s 237-238
+	port_read(HD_DATA,CURRENT->buffer,256);     // 读数据到请求结构缓冲区
+	CURRENT->errors = 0;                        // 清出错次数
+	CURRENT->buffer += 512;                     // 调整缓冲区指针，指向新的空区
+	CURRENT->sector++;                          // 起始扇区号加1
+	if (--CURRENT->nr_sectors) {                // 如果所需读取的扇区数还没读完，则再
+		do_hd = &read_intr;                     // 置硬盘调用C函数指针为read_intr()
 		return;
 	}
-	end_request(1);
+// 执行到此，说明本次请求项的全部扇区数据已经读完，则调用end_request()函数去处理请求
+// 项结束事宜。最后再次调用do_hd_request()，去处理其他硬盘请求项。执行其他硬盘请求操作。
+	end_request(1);                             // 数据已更新标志置位(1)
 	do_hd_request();
 }
 
+//// 写扇区中断调用函数
+// 该函数将在硬盘写命令结束时引发的硬盘中断过程中被调用。该函数功能与read_intr()类似。
+// 在写命令执行后会产生硬件中断信号，并执行硬件中断处理程序，此时在硬盘中断处理程序中
+// 调用的C函数指针do_hd已经指向了write_intr()，因此会在一次写扇区操作完成（或出错）
+// 后就会执行该函数。
 static void write_intr(void)
 {
-	if (win_result()) {
-		bad_rw_intr();
-		do_hd_request();
+// 该函数首先判断此次写命令操作是否出错。若命令结束后控制器还处于忙状态，或者命令
+// 执行出错，则处理硬盘操作失败问题，接着再次请求硬盘作复位处理并执行其他请求项，
+// 然后返回。在bad_rw_intr()函数中，每次操作出错都会对当前请求项做出错次数累计，
+// 若出错次数不到最大允许出错次数的一半，则先执行硬盘复位操作，然后再执行本次请求项
+// 处理。若出错次数已经大于等于最大允许出错次数MAX_ERRORS(7次)，则结束本次请求项
+// 的处理而去处理队列中下一个请求项。do_hd_request()中会根据当时具体的标志状态来
+// 判别是否需要先执行复位、重新校正等操作，然后再继续或处理下一个请求项。
+	if (win_result()) {             // 如果硬盘控制器返回错误信息，
+		bad_rw_intr();              // 则首先进行硬盘读写失败处理
+		do_hd_request();            // 再次请求硬盘作相应(复位)处理
 		return;
 	}
-	if (--CURRENT->nr_sectors) {
-		CURRENT->sector++;
-		CURRENT->buffer += 512;
-		do_hd = &write_intr;
-		port_write(HD_DATA,CURRENT->buffer,256);
+// 此时说明本次写一扇区操作成功，因此将欲写扇区数减1。若其不为0，则说明还有扇区
+// 要写，于是把当前请求起始扇区号+1，并调整请求项数据缓冲区指针指向下一块欲写的
+// 数据。然后再重置硬盘中断处理程序调用的C函数指针do_hd(指向本函数)。接着向控制器
+// 数据端口写入512字节数据，然后函数返回去等待控制器把这些数据写入硬盘后产生的中断
+	if (--CURRENT->nr_sectors) {    // 若还有扇区要写，则
+		CURRENT->sector++;          // 当前请求起始扇区号+1，
+		CURRENT->buffer += 512;     // 调整请求缓冲区指针
+		do_hd = &write_intr;        // 置函数指针为write_intr()
+		port_write(HD_DATA,CURRENT->buffer,256);    // 向数据端口写256字。
 		return;
 	}
-	end_request(1);
-	do_hd_request();
+// 若本次请求项的全部扇区数据已经写完，则调用end_request()函数去处理请求项结束事宜。
+// 最后再次调用do_hd_request()，去处理其他硬盘请求项。执行其他硬盘请求操作。
+	end_request(1);                 // 处理请求结束事宜。
+	do_hd_request();                // 执行其他硬盘请求操作。
 }
 
+//// 硬盘重新校正(复位)中断调用函数
+// 该函数会在硬盘执行重新校正操作而引发的硬盘中断中被调用。
+// 如果硬盘控制器返回操作信息，则函数首先进行硬盘读写失败处理，然后请求硬盘作相应
+// (复位)处理。在bad_rw_intr()函数中，每次操作出错都会对当前请求项做出错次数累计，
+// 若出错次数不到最大允许出错次数的一半，则会先执行硬盘复位操作，然后再执行本次
+// 请求项处理。若出错次数已经大于等于最大允许出错次数MAX_ERRORS(7次)，则结束本次
+// 请求项的处理而去处理队列中下一个请求项。do_hd_request()中会根据当时具体的标志
+// 状态来判别是否需要先执行复位、重新校正等操作，然后再继续或处理下一请求项。
 static void recal_intr(void)
 {
-	if (win_result())
+	if (win_result())       // 若返回出错，则调用bad_rw_intr()
 		bad_rw_intr();
 	do_hd_request();
 }
 
+//// 执行硬盘读写请求操作
+// 
 void do_hd_request(void)
 {
 	int i,r;
